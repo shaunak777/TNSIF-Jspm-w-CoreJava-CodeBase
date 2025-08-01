de514aaf56478fb4c16dbe2c4b68599ad2ff1133
shaunak777 <shaunakshamraj09@gmail.com>
2025-08-01 13:24:36 +0530

    Updated Day one

--- Day_1/src/com/tnsif/dayone/FirstProgram.java
+++ Day_1/src/com/tnsif/dayone/FirstProgram.java
@@ -3,11 +3,51 @@ package com.tnsif.dayone;
 public class FirstProgram {
 	public static void main(String args[])
 	{
-		System.out.println("hello Shaunak");
-		int a=4;
-		int b=5;
-		int add=a+b;
-		System.out.print(add);
+		
+		
+		//System.out.println("hello Shaunak");
+	int a=4;
+		int b=5;		int add=a+b;
+	//	System.out.print(add);	
+		//variables declaration-Invalid identifier
+		// int for=1
+		//int num 1=13;
+		
+		//valid identifier
+		//datatype varname =data
+				
+		int $num=12;
+		System.out.println("value of num is :"+$num);
+		String StudentName="Shaunak";
+		System.out.println("Value of name is :"+StudentName);
+		int value=9/2;
+		System.out.println(value);
+		float value2=101f/61f;
+		System.out.println(value2);
+		
+		double value3=101d/61d;
+		System.out.println(value3);
+		
+		
+		int radius=10;
+		
+		double percentage=3.14*radius*radius;
+		System.out.println("Percentage:"+percentage);
+		
+		
+		char ch='a';
+		System.out.println(ch);
+		
+		char ch1=65;
+		System.out.println(ch1);
+		
+		
+		
+	    			
+		
+		
+		
+		
 	}
 
 }
