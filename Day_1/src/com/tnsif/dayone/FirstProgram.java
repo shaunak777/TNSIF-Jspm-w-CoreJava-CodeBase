a3a02f4a9f338fd766f4b395ba259190a7d2beec
shaunak777 <shaunakshamraj09@gmail.com>
2025-08-01 12:32:03 +0530

    Variables

--- Day_1/src/com/tnsif/dayone/FirstProgram.java
+++ Day_1/src/com/tnsif/dayone/FirstProgram.java
@@ -4,6 +4,10 @@ public class FirstProgram {
 	public static void main(String args[])
 	{
 		System.out.println("hello Shaunak");
+		int a=4;
+		int b=5;
+		int add=a+b;
+		System.out.print(add);
 	}
 
 }
