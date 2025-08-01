f5922e3705c5861a112553992bdcbf053fa776fc
shaunak777 <shaunakshamraj09@gmail.com>
2025-08-01 13:26:42 +0530

    Updated

---
+++ Day_1/src/com/tnsif/dayone/PrimitiveDatatypeDemo.java
@@ -0,0 +1,21 @@
+package com.tnsif.dayone;
+
+public class PrimitiveDatatypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          byte byteMax=126;
+          byte byteMin=-128 ;
+          System.out.println("Min range of byte"+byteMin);
+          System.out.println("Min range of byte"+byteMax);
+          //short 2 byte
+          short shortMax=32767;
+          
+          short ShortMin=-32768;
+          
+          int intmax= 2147483647;
+          int intMin= -2147483648;		  
+          
+          //for long
+          
+         
